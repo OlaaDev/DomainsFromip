10723a322f585c0c9476069ac56d936808a24f78
unCoder <ahmedsadam633@gmail.com>
2022-01-21 21:09:27 +0100

    Initial commit

---
+++ Domainsip/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(275, 25)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Load List"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 43)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(275, 25)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 97)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(275, 341)
+        Me.TextBox1.TabIndex = 2
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 74)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(93, 17)
+        Me.RadioButton1.TabIndex = 3
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Domains To ip"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(111, 74)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(93, 17)
+        Me.RadioButton2.TabIndex = 4
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "ip To Domains"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 450)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+End Class
---
+++ Domainsip/Form1.vb
@@ -0,0 +1,62 @@
+﻿Imports System.IO, Leaf.Net
+Imports System.Threading
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim XLIST As String()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim open As New OpenFileDialog() With {.Title = "Load List Ip", .Multiselect = False}
+        If open.ShowDialog = DialogResult.OK Then
+            XLIST = File.ReadAllLines(open.FileName)
+        End If
+    End Sub
+    Sub Checker(ByVal ip As String)
+        Dim https As New HttpRequest
+        https.UserAgent = Http.RandomUserAgent
+        Dim response As String = https.Get("https://sonar.omnisint.io/reverse/" + ip).ToString
+        Dim match As MatchCollection = Regex.Matches(response, """(.*?)""")
+        Dim x As String
+        For Each olaa As Object In match
+            Dim math As Match = CType(olaa, Match)
+            Dim domain = math.Groups(1).Value.ToString
+            TextBox1.AppendText(domain + vbCrLf)
+            x += domain + vbCrLf
+        Next
+        File.AppendAllText(ip + ".txt", x)
+    End Sub
+    Sub geter(ByVal domain As String)
+        Try
+            Dim https As New HttpRequest
+            https.UserAgent = Http.RandomUserAgent
+            Dim response As String = https.Post("https://check-host.net/ip-info?host=" + domain).ToString
+            Dim ip As String = Regex.Match(response, "<td><strong>(.*?)</strong></td>").Groups(1).Value
+            TextBox1.AppendText(ip + vbCrLf)
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Thread As New Thread(Sub()
+                                     Try
+                                         If RadioButton2.Checked Then
+                                             For Each ip In XLIST
+                                                 Checker(ip)
+                                             Next
+                                         End If
+                                         If RadioButton1.Checked Then
+                                             For Each domain In XLIST
+                                                 geter(domain)
+                                             Next
+                                         End If
+                                     Catch ex As Exception
+
+                                     End Try
+
+                                 End Sub)
+        Thread.IsBackground = True
+        Thread.Start()
+    End Sub
+End Class
